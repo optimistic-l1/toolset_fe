b49c559c172fff22f251c3559f0b7dea765f37da
luoziheng <luoziheng@genomics.cn>
2025-01-15 16:06:05 +0800

    modify:修改deploy.yml脚本

--- quasar.conf.js
+++ quasar.conf.js
@@ -48,6 +48,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+      distDir: 'dist',
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
